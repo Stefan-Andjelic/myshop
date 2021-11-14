52a3de2c10bfa7aaf9e3dda2abf9789a948dcf11
Stefan <stefanandjelic8@gmail.com>
2021-11-13 18:17:49 -0600

    Built shopping cart (views, templates, url, context processor)

--- cart/cart.py
+++ cart/cart.py
@@ -4,59 +4,28 @@ from shop.models import Product
 
 
 class Cart(object):
+
     def __init__(self, request):
         """
-        Initalize the cart.
+        Initialize the cart.
         """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
-
         if not cart:
-            # Save an empty cart in the session
+            # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
-
         self.cart = cart
 
-    def add(self, product, quantity=1, override_quantity=False):
+    def __iter__(self):
         """
-        Add a product to the cart or update its quantity.
-        """
-        product_id = str(product.id)
-
-        if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
-
-        if override_quantity:
-            self.cart[product_id]['quantity'] = quantity
-
-        else:
-            self.cart[product_id]['quantity'] += quantity
-        
-        self.save()
-
-    def save(self):
-        # Mark the session as "modified" to make sure it gets saved
-        self.session.modified = True
-
-    def remove(self, product):
-        """
-        Remove product from the cart.
-        """
-        product_id = str(product.id)
-
-        if product_id in self.cart:
-            del self.cart[product_id]
-            self.save()
-
-    def __iter__(self, product):
-        """
-        Iterate over the items in the cart and get the products from the database.
+        Iterate over the items in the cart and get the products
+        from the database.
         """
         product_ids = self.cart.keys()
-        # Get the product objects and add them to the cart
+        # get the product objects and add them to the cart
         products = Product.objects.filter(id__in=product_ids)
+
         cart = self.cart.copy()
-        
         for product in products:
             cart[str(product.id)]['product'] = product
 
@@ -71,12 +40,37 @@ class Cart(object):
         """
         return sum(item['quantity'] for item in self.cart.values())
 
-    def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+    def add(self, product, quantity=1, override_quantity=False):
+        """
+        Add a product to the cart or update its quantity.
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                      'price': str(product.price)}
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        # mark the session as "modified" to make sure it gets saved
+        self.session.modified = True
+
+    def remove(self, product):
+        """
+        Remove a product from the cart.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
 
     def clear(self):
-        """
-        Remove cart from session.
-        """
+        # remove cart from session
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
